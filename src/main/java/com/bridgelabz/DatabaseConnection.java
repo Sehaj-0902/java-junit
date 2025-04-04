adf5481059b7a13fb7d679911089027b1c786649
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-04-04 11:56:13 +0530

    [Sehaj] ADD: Program to test @BeforeEach and @AfterEach annotations

---
+++ src/main/java/com/bridgelabz/DatabaseConnection.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class DatabaseConnection {
+    // Method to connect
+    public String connect() {
+        return "true";
+    }
+
+    // Method to disconnect
+    public String disconnect() {
+        return "true";
+    }
+}
